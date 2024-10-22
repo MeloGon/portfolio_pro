ecdd8667e4cd13435623adc5d27816b7cb682514
Kevyn Melo <troy.esname@gmail.com>
2024-10-22 13:57:13 -0500

    setup web deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:portfolio/config/config.dart';
 import 'package:portfolio/presentation/providers/state.dart';
 
-void main() => runApp(const ProviderScope(child: MyApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyAb5aby0k1uTudlJVEKTbUqHqhlLTg6Gy4",
+    appId: "1:173175341099:web:33e6dada6b8e3d56ad6a6e",
+    messagingSenderId: "173175341099",
+    projectId: "portfolio-266cc",
+  ));
+  runApp(ProviderScope(child: MyApp()));
+}
 
 class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
