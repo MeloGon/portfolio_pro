465ec31e252755f6c92816e92300430d68073fd8
Kevyn Melo Gonzales <troy.esname@gmail.com>
2024-11-11 16:50:15 -0500

    fonts were added

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -8,11 +8,11 @@ class AppTheme {
   AppTheme({required this.isDarkmode});
 
   ThemeData getTheme() => ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: seedColor,
-        brightness: isDarkmode ? Brightness.dark : Brightness.light,
-        listTileTheme: const ListTileThemeData(
-          iconColor: seedColor,
-        ),
-      );
+      useMaterial3: true,
+      colorSchemeSeed: seedColor,
+      brightness: isDarkmode ? Brightness.dark : Brightness.light,
+      listTileTheme: const ListTileThemeData(
+        iconColor: seedColor,
+      ),
+      fontFamily: 'HostGrotesk');
 }
